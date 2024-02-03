00a841a932f460fbff828b338dd610e1689385b0
Kleist Bond <61907235+KleistvonLiu@users.noreply.github.com>
2024-02-03 22:37:32 +0800

    Update vector.h

--- MyTinySTL/vector.h
+++ MyTinySTL/vector.h
@@ -47,8 +47,8 @@ public:
   // vector 的嵌套型别定义
   typedef mystl::allocator<T>                      allocator_type;
   typedef mystl::allocator<T>                      data_allocator;
-
-  typedef typename allocator_type::value_type      value_type;
+  
+  typedef typename allocator_type::value_type      value_type; // jf: 这里的typename是告诉编译器 value_type是allocator_type 中的一个类型，而不是它的静态成员变量
   typedef typename allocator_type::pointer         pointer;
   typedef typename allocator_type::const_pointer   const_pointer;
   typedef typename allocator_type::reference       reference;
@@ -70,7 +70,7 @@ private:
 
 public:
   // 构造、复制、移动、析构函数
-  vector() noexcept
+  vector() noexcept // 声明不会抛出异常，如果抛出会调用std::terminate
   { try_init(); }
 
   explicit vector(size_type n)
@@ -80,7 +80,7 @@ public:
   { fill_init(n, value); }
 
   template <class Iter, typename std::enable_if<
-    mystl::is_input_iterator<Iter>::value, int>::type = 0>
+    mystl::is_input_iterator<Iter>::value, int>::type = 0> //std::enable_if如果接受的一个参数为true，就定义type为第二个参数; =0表示默认值？
   vector(Iter first, Iter last)
   {
     MYSTL_DEBUG(!(last < first));
